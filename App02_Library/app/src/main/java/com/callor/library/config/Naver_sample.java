d60f0c3d4b0987e99c8cba89155b7a7568fed7e4
paldonenemttin <aipro0124@naver.com>
2021-08-06 11:08:33 +0900

    sample

--- App02_Library/app/src/main/java/com/callor/library/config/Naver_sample.java
+++ App02_Library/app/src/main/java/com/callor/library/config/Naver_sample.java
@@ -3,7 +3,7 @@ package com.callor.library.config;
 /**
  * naver open api 연동을 위한 아이디와 비밀번호
  */
-public class Naver {
+public class Naver_sample {
 
     public static String CLIENT_ID = "YOUR NAVER CLIENT ID";
     public static String CLIENT_SECRET = "";
